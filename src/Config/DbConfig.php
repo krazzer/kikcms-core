6d8d9ebd5efadf32159b2852df8c9d2218b88c45
Kaz van Wel <info@kiksaus.nl>
2019-09-04 08:48:09 +0200

    Show object name in datatable header

--- src/Config/DbConfig.php
+++ src/Config/DbConfig.php
@@ -8,6 +8,8 @@ class DbConfig
     const ERROR_CODE_FK_CONSTRAINT_FAIL        = 1451;
     const ERROR_CODE_TOO_MANY_USER_CONNECTIONS = 1226;
 
+    const NAME_FIELD = 'name';
+
     const SQL_DATE_FORMAT     = 'Y-m-d';
     const SQL_DATETIME_FORMAT = 'Y-m-d H:i:s';
     const SQL_TIME_FORMAT     = 'H:i:s';
