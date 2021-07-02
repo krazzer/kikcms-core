6e02fc7a61edd7a5c147a426dca711850eec2f65
Kaz van Wel <info@kiksaus.nl>
2021-07-02 09:17:35 +0200

    Fix relationkey problem by setting reusable option on my default for relations

--- src/Classes/Model.php
+++ src/Classes/Model.php
@@ -266,6 +266,8 @@ class Model extends \Phalcon\Mvc\Model
      */
     private function updateDefaults(array $options): array
     {
+        $options['reusable'] = true;
+
         if ( ! isset($options['defaults'])) {
             return $options;
         }
