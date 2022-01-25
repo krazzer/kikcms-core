8658478ca19a922c75a964f3ae4656c06f2b2876
Kaz van Wel <info@kiksaus.nl>
2022-01-25 08:49:11 +0100

    Fix unsetting a relation

--- src/Classes/Model.php
+++ src/Classes/Model.php
@@ -255,7 +255,7 @@ class Model extends \Phalcon\Mvc\Model
      */
     public function unsetRelation(string $alias)
     {
-        unset($this->_related[strtolower($alias)]);
+        unset($this->dirtyRelated[strtolower($alias)]);
     }
 
     /**
